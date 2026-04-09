53901530e62f9c82e491d79d3fbedeb4e22c4c67
root <root@nby-test2-0.nby-test2-service.ad-e2e.svc.cluster.local>
2026-04-09 15:09:32 +0000

    update caching script for multi-node support; add internvl3 8b finetune script

--- scripts/cache_dataset/run_caching_recogdrive_hidden_state.sh
+++ scripts/cache_dataset/run_caching_recogdrive_hidden_state.sh
@@ -1,31 +1,36 @@
 TRAIN_TEST_SPLIT=navtrain
 export NUPLAN_MAP_VERSION="nuplan-maps-v1.0"
 export NUPLAN_MAPS_ROOT="/mnt/volumes/ad-e2e-al-sh01/jiaoqf/recogdrive/download/maps/nuplan-maps-v1.0"
-export NAVSIM_EXP_ROOT="/mnt/volumes/ad-e2e-al-sh01/jiaoqf/recdrive/exp"
-
-# export NAVSIM_EXP_ROOT="/mnt/volumes/ad-e2e-al-sh01/nby/recdrive/exp"
-export NAVSIM_DEVKIT_ROOT="/mnt/volumes/ad-e2e-al-sh01/cy/recdrive"
+export NAVSIM_EXP_ROOT="/mnt/volumes/ad-e2e-al-sh01/nby/recdrive/exp"
+export NAVSIM_DEVKIT_ROOT="/workspace/code"
 export OPENSCENE_DATA_ROOT="/mnt/volumes/ad-e2e-al-sh01/jiaoqf/recogdrive/download"
-CACHE_PATH=$NAVSIM_EXP_ROOT/recogdrive_agent_cache_dir_train_jiaoqf
-#CACHE_PATH=$NAVSIM_EXP_ROOT/recogdrive_agent_cache_dir_train_nby
+CACHE_PATH=$NAVSIM_EXP_ROOT/recogdrive_agent_cache_dir_train
 
 export NCCL_IB_DISABLE=0
 export NCCL_P2P_DISABLE=0
 export NCCL_SHM_DISABLE=0
 export PYTHONPATH="${NAVSIM_DEVKIT_ROOT}${PYTHONPATH:+:${PYTHONPATH}}"
 
-MASTER_PORT=${MASTER_PORT:-63669}
-PORT=${PORT:-63665}
-GPUS=${GPUS:-8}
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-NODES=$((GPUS / GPUS_PER_NODE))
-export MASTER_PORT=${MASTER_PORT}
-export PORT=${PORT}
+NNODES="${WORLD_SIZE:-1}"
+NODE_RANK="${RANK:-0}"
+MASTER_ADDR="${MASTER_ADDR:?MASTER_ADDR is empty}"
+MASTER_PORT="${MASTER_PORT:-63669}"
+GPUS_PER_NODE="${GPUS:-8}"
 
-echo "GPUS: ${GPUS}"
+export MASTER_ADDR=${MASTER_ADDR}
+export MASTER_PORT=${MASTER_PORT}
+
+echo "MASTER_ADDR: ${MASTER_ADDR}"
+echo "MASTER_PORT: ${MASTER_PORT}"
+echo "NNODES: ${NNODES}"
+echo "NODE_RANK: ${NODE_RANK}"
 echo "GPUS_PER_NODE: ${GPUS_PER_NODE}"
 
-torchrun \
+/mnt/volumes/ad-e2e-al-sh01/nby/recdrive/conda_envs/recdrive/bin/torchrun \
+    --nnodes=${NNODES} \
+    --node_rank=${NODE_RANK} \
+    --master_addr=${MASTER_ADDR} \
+    --master_port=${MASTER_PORT} \
     --nproc_per_node=${GPUS_PER_NODE} \
     $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_dataset_caching_multi_node.py \
     agent=recogdrive_agent \
@@ -34,7 +39,7 @@ torchrun \
     agent.cache_hidden_state=True \
     agent.cache_mode=True \
     train_test_split=$TRAIN_TEST_SPLIT \
-    agent.vlm_path="/mnt/volumes/ad-e2e-al-sh01/nby/recdrive/ReCogDrive-VLM-2B" \
+    agent.vlm_path="/workspace/volumes/ad-e2e-al-sh01/nby/recdrive/ReCogDrive-VLM-2B" \
     cache_path=$CACHE_PATH \
-    worker=sequential
-    #> /mnt/volumes/ad-e2e-al-sh01/cy/log/caching_dataset.txt 2>&1
+    worker=sequential \
+    > /mnt/volumes/ad-e2e-al-sh01/nby/log/caching_dataset_rank${NODE_RANK}.txt 2>&1
